d13d7e8d36ce5a795e9382f40112d705b6b84fa9
QiangGao <gqiang3@163.com>
2019-02-26 02:01:23 +0800

    Initial commit

---
+++ src/main/java/cn/vuclip/read/ReadApplication.java
@@ -0,0 +1,13 @@
+package cn.vuclip.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadApplication.class, args);
+    }
+
+}
